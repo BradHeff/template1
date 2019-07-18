61b63f574c92374f809893c199d3cae15ee69ed7
pheonix <ph3onix83@gmail.com>
2019-07-18 15:02:17 +0930

    Added Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-  render() {
-     return (
-        <div className="header after-overlay-6 bg-rules">
-            <div className="App-header">
-                <div className="container">
-                    <h1>Title</h1>
-                    <p>Paragraph</p>
-                    <a href="#0" className="btn btn-primary px-4 py-2">Click Me</a>
+    btnClick = (e: React.SyntheticEvent<HTMLAnchorElement>) => {
+        e.preventDefault();
+        console.log("Clicked Header Button");
+    }
+    render() {
+        return (
+            <div className="header overlay bg-rules">
+                <div className="App-header">
+                    <div className="container z-index">
+                        <h1>Title</h1>
+                        <p>Paragraph</p>
+                        <a href="#0" onClick={this.btnClick} className="btn btn-primary px-4 py-2">Click Me</a>
+                    </div>
                 </div>
             </div>
-           
-        </div>
-     );
-  }
+        );
+    }
 }
 
 export default Header;
