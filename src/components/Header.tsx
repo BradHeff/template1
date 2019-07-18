cf41cbab9176b90a253e4fef9449caea77c7be59
pheonix <ph3onix83@gmail.com>
2019-07-18 14:50:18 +0930

    Added Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+     return (
+        <div className="header after-overlay-6 bg-rules">
+            <div className="App-header">
+                <div className="container">
+                    <h1>Title</h1>
+                    <p>Paragraph</p>
+                    <a href="#0" className="btn btn-primary px-4 py-2">Click Me</a>
+                </div>
+            </div>
+           
+        </div>
+     );
+  }
+}
+
+export default Header;
